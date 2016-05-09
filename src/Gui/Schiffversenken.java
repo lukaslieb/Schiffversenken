48b333ec3d72b7712080af77a8aa62aee2eb953c
Chapmes <sandro_9_2@hotmail.com>
2016-05-09 11:09:15 +0200

    GUI with Buttons
    John Snow is alive

--- src/Gui/Schiffversenken.java
+++ src/Gui/Schiffversenken.java
@@ -7,20 +7,29 @@ package Gui;
 
 import Network.ClientThread;
 import Network.ServerThread;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Lukas
  */
-public class Schiffversenken extends JFrame implements ActionListener{
+public class Schiffversenken extends JFrame implements ActionListener, MouseListener{
 
     private JMenuBar mBar = new JMenuBar();
     private JMenu mFile = new JMenu("File");
@@ -34,10 +43,24 @@ public class Schiffversenken extends JFrame implements ActionListener{
     private JMenuItem mExit = new JMenuItem("Exit");
     private JMenuItem mAbout = new JMenuItem("About");
     
+    private JLabel player1 = new JLabel();
+    private JLabel player2 = new JLabel();
+    private JLabel Titel1 = new JLabel();
+    private JLabel Titel2 = new JLabel();
+    private JPanel leftTopPanel = new JPanel();
+    private JPanel rightTopPanel = new JPanel();
+    private JPanel leftPanel = new JPanel();
+    private JPanel rightPanel = new JPanel();
+    private JButton butLeft[][] = new JButton[5][5];
+    private JButton butRight[][] = new JButton[5][5];
+    private ArrayList<JButton> left = new ArrayList<JButton>();
+
+    
     public Schiffversenken(){ 
         super();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(500,500);
+        setSize(700,600);
+        setLayout(new GridLayout(2, 2));
         
         mNewLocal.add(mLocalHuman);
         mNewLocal.add(mLocalComp);
@@ -63,6 +86,58 @@ public class Schiffversenken extends JFrame implements ActionListener{
         mLocalComp.addActionListener(this);
         mNetHost.addActionListener(this);
         mNetClient.addActionListener(this);
+        
+        add(leftTopPanel);
+        add(rightTopPanel);
+        add(leftPanel);
+        add(rightPanel);
+
+        leftPanel.setLayout(new GridLayout(5, 5, 0, 0));
+        rightPanel.setLayout(new GridLayout(5, 5, 0, 0));
+        leftTopPanel.setLayout(new BorderLayout());
+        rightTopPanel.setLayout(new BorderLayout());
+        
+        leftTopPanel.add(Titel1, BorderLayout.NORTH);
+        Titel1.setText("SCHIFFE    ");
+        Titel1.setFont(Titel1.getFont().deriveFont(40.0f));
+        Titel1.setHorizontalAlignment(JLabel.RIGHT);
+        
+        rightTopPanel.add(Titel2, BorderLayout.NORTH);
+        Titel2.setText("   VERSENKEN");
+        Titel2.setFont(Titel1.getFont().deriveFont(40.0f));
+        Titel2.setHorizontalAlignment(JLabel.LEFT);
+        
+        leftTopPanel.add(player1, BorderLayout.SOUTH );
+        player1.setText("Player 1");
+        player1.setFont(Titel1.getFont().deriveFont(25.0f));
+        player1.setHorizontalAlignment(JLabel.CENTER);
+        
+        rightTopPanel.add(player2, BorderLayout.SOUTH );
+        player2.setText("Player 2");
+        player2.setFont(Titel1.getFont().deriveFont(25.0f));
+        player2.setHorizontalAlignment(JLabel.CENTER);
+        
+
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                butLeft[row][col] = new JButton();
+                butLeft[row][col].addMouseListener(this);
+                butLeft[row][col].setBackground(Color.white);
+                leftPanel.add(butLeft[row][col]);
+                left.add(butLeft[row][col]);
+            }
+        }
+
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                butRight[row][col] = new JButton();
+                butRight[row][col].addMouseListener(this);
+                butRight[row][col].setBackground(Color.blue);
+                rightPanel.add(butRight[row][col]);
+            }
+        }
+
+        setVisible(true);
     }
 
     @Override
@@ -115,5 +190,34 @@ public class Schiffversenken extends JFrame implements ActionListener{
                 dialog.setVisible(true);
             }
         }
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (left.contains(e.getComponent())) {
+            e.getComponent().setBackground(Color.black);
+        }
+        else{
+            e.getComponent().setBackground(Color.red);
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
     }
 }
