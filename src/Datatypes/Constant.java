6ac607796a00fb5ac6fc7ad33800757e67121ab5
Chapmes <sandro_9_2@hotmail.com>
2016-05-10 16:46:19 +0200

    Klasse mit Konstanten hinzugefügt
    Klasse Constant im package Datatypes hinzugefugt
    2 konstanten: fieldsize & port

---
+++ src/Datatypes/Constant.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Datatypes;
+
+/**
+ *
+ * @author Sandro
+ */
+public class Constant {
+    public static final int fieldSize = 10;
+    public static final int port = 1337;
+    
+}
