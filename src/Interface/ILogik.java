615a4efbd8edd3676d653e27461422f201f6c9ec
lukaslieb <lukaslieb@hotmail.ch>
2016-05-10 15:35:33 +0200

    Add Enemy Interface and FieldStatus enum
    Add Enemy Interface for Network player or KI
    Add enum FieldStatus for the status of the field with getColor function
    for this status

--- src/Interface/ILogik.java
+++ src/Interface/ILogik.java
@@ -6,9 +6,15 @@
 package Interface;
 
 /**
- *
+ * This interface define the Methods how communicate with the logic.
  * @author Lukas
  */
 public interface ILogik {
-    
+    /**
+     * This method tells the logic on witch field the player has shoot.
+     * @param x = x coordinate of the field
+     * @param y = y coordinate of the field
+     * @return true if the shoot was acceptet from the logic
+     */
+    boolean shoot(int x, int y);
 }
--- src/Interface/ISpielfeld.java
+++ src/Interface/ISpielfeld.java
@@ -4,11 +4,20 @@
  * and open the template in the editor.
  */
 package Interface;
+import Datatypes.FieldStatus;
 
 /**
- *
+ * This interface define the methods how communicate with the Spielfeld Gui.
  * @author Lukas
  */
 public interface ISpielfeld {
+    /**
+     * Method for updating the actual Field. 
+     * @param x = x Coordinate of Field
+     * @param y = y Coordinate of Field
+     * @param status = status of Field (enum FieldStatus)
+     */
+    void updateField(int x, int y, FieldStatus status); 
+    
     
 }
