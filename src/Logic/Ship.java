5570937cdf17bc5e91ec98c4a3d8b8ab35a5b888
hsluhediger <felix.hediger@stud.hslu.ch>
2016-05-17 11:28:55 +0200

    ship update
    ship update

--- src/Logic/Ship.java
+++ src/Logic/Ship.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package Logic;
+import static Datatypes.Constant.fieldSize;
 import Datatypes.FieldStatus;
 import Datatypes.ShipAlignment;
 
@@ -16,22 +17,66 @@ public class Ship {
     private int y;
     private ShipAlignment align;
     private int length;
-    
+    private ShipFields[] placement;
     
     public Ship(int x, int y, ShipAlignment align, int length){
         this.x = x;
         this.y = y;
         this.align = align;
         this.length = length;
+        checkPos();
         CreateShip();
+        
     }
+
     
-    private void CreateShip(){
+    
+    private void checkPos(){
         switch(align){
             case VERTICAL:
+                if(!((y+length) < fieldSize)){
+                    y=fieldSize-length-1;
+                }
+                break;
+            case HORIZONTAL:
+                if(!((x+length) < fieldSize)){
+                    x=fieldSize-length-1;
+                }
+                break;
+        }
+    } 
+            
+    private void CreateShip(){
+        
+        switch(align){
+            case VERTICAL:
+                placement= new ShipFields[length];
+                for(int i=0; i<length; i++)
+                {
+                    placement[i]=new ShipFields()
+                }
                 break;
             case HORIZONTAL:
                 break;
         }
+        
+               
+       
+    }
+    public boolean isCollision(int x, int y){
+        //TODO abfrage ob kollision
+        
+    }
+    public FieldStatus shootShip(int x,int y){
+        //TODO allreadyhit, hit
+        
+        return FieldStatus.UNKNOWNAREA;
+    }
+    public boolean isDestroyed(){
+        
+        //TODO true, false
+    }
+    public ShipFields[] returnShipFields (){
+        return placement;
     }
 }
